5a44787b35430053a0cb6b1c8b3b1f8a69aa0ce1
CaoTuan <caotuan92.hus@gmail.com>
2024-11-22 16:01:49 +0700

    init test java selenium

---
+++ TestJava/src/Main.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args){
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://demo.guru99.com/");
+        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
+        element.sendKeys("abc@gmail.com");
+
+        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
+        button.click();
+    }
+}
