fd0c5e9986c7c6a3de9c5104fc02340bb927409e
CaoTuan <caotuan92.hus@gmail.com>
2024-11-23 10:14:53 +0700

    update code

--- TestJava/src/Main.java
+++ TestJava/src/Main.java
@@ -1,15 +1,28 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
     public static void main(String[] args){
-        ChromeDriver driver=new ChromeDriver();
-        driver.get("https://demo.guru99.com/");
-        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
-        element.sendKeys("abc@gmail.com");
+        String profilePath = "C:\\Users\\CaoTuan\\AppData\\Local\\Google\\Chrome\\User Data";
+        String profileNameClone = "Profile 1";
+        String profileNameMain = "Default";
 
-        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
-        button.click();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("user-data-dir=" + profilePath);
+        options.addArguments("profile-directory=" + profileNameMain);
+
+        ChromeDriver driver=new ChromeDriver(options);
+
+        driver.get("https://demo.guru99.com/");
+        driver.quit();
+
+//        driver.get("https://demo.guru99.com/");
+//        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
+//        element.sendKeys("abc@gmail.com");
+//
+//        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
+//        button.click();
     }
 }
