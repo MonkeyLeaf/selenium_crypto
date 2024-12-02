5366e09c58e551dd78e3f4990a978912fe46a078
CaoTuan <caotuan92.hus@gmail.com>
2024-12-02 14:50:14 +0700

    update code

--- TestJava/src/Main.java
+++ TestJava/src/Main.java
@@ -1,7 +1,9 @@
+import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Main {
     public static void main(String[] args){
@@ -9,13 +11,8 @@ public class Main {
         String profileNameClone = "Profile 1";
         String profileNameMain = "Default";
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("user-data-dir=" + profilePath);
-        options.addArguments("profile-directory=" + profileNameMain);
-
-        ChromeDriver driver=new ChromeDriver(options);
-
-        driver.get("https://demo.guru99.com/");
+        WebDriverManager.chromedriver().driverVersion("131.0.6778.86").clearDriverCache().setup();
+        ChromeDriver driver = getChromeDriver(profilePath, profileNameClone);
         driver.quit();
 
 //        driver.get("https://demo.guru99.com/");
@@ -24,5 +21,24 @@ public class Main {
 //
 //        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
 //        button.click();
+    }
+
+    @NotNull
+    private static ChromeDriver getChromeDriver(String profilePath, String profileNameClone) {
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--no-sandbox"); // Bỏ qua sandbox để tránh hạn chế
+        options.addArguments("--disable-dev-shm-usage"); // Giảm thiểu việc sử dụng shared memory
+        options.addArguments("--disable-gpu"); // Tắt GPU (trong môi trường không có đồ họa)
+        options.addArguments("--headless"); // Chạy trong chế độ không giao diện (nếu cần)
+
+
+        options.addArguments("user-data-dir=" + profilePath);
+        options.addArguments("profile-directory=" + profileNameClone);
+
+        ChromeDriver driver=new ChromeDriver(options);
+
+        driver.get("https://demo.guru99.com/");
+        return driver;
     }
 }
